3446441131849f1806b810f25042e511543836fc
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2020-12-28 19:27:00 +0200

    portfolio

---
+++ frontend/src/utils/project-list.js
@@ -0,0 +1,52 @@
+let project_list = [
+	{
+		image:
+			'https://media.discordapp.net/attachments/763317063266729986/793144962908815380/unknown.png?width=1025&height=471',
+		title: 'Online School System',
+		des:
+			"full Stack Application using the python Django in the backend, react and redux in the frontend, it’s about manage a School system, it has adminwith all control, portal for Students/Parents, Staff, Teachers, Classes, Subjects and Exams. ",
+		tags: [ 'React', 'Redux','typeScript','Django','Firebase','Cloudenary' ],
+		links: {
+			launch: 'https://online-clothing-store.herokuapp.com/',
+			github: 'https://github.com/ameedasmah/OnlineSchoolSystem'
+		}
+	},
+	{
+		image:
+			'https://media.discordapp.net/attachments/763317063266729986/793145544268578816/unknown.png?width=911&height=473',
+		title: 'HotelCom',
+		des:
+			"A MERN-stack project that allows you use a hotels API prewiring the hotels of a specific city and country and being able to add it toBookmarks and add favorites, and in the profile page you will be able to make payments throw Stripe API, there also authentication and authorization using JWT and hashing the password using bcrybt.",
+		tags: ['nodejs',"ReactJ","Express","mongoDb","jwt","Api" ],
+		links: {
+			launch: 'https://raptors-hotels.herokuapp.com/',
+			github: 'https://github.com/ameedasmah/green-field-project'
+		}
+	},
+	{  /* -------------------------------- */
+		image:
+			'https://media.discordapp.net/attachments/762737273128353841/792798697003548732/Capture11.png?width=833&height=473',
+		title: 'Laflefne',
+		des:
+			"MERN Stack Application with JWT authentication letting users to see the places in Palestine, and to book a trip, the users who want to book the same trip can make  chat with each other. ",
+		tags: [ 'React', 'Express', 'mongoDb','Nodejs' ],
+		links: {
+			// launch: 'https://faizhameed.github.io/emailChips/',
+			github: 'https://github.com/ameedasmah/laflefne'
+		}
+	},
+	{/* -------------------------------- */
+		image:
+			'https://media.discordapp.net/attachments/763317063266729986/793141749849522186/unknown.png?width=1025&height=472',
+		title: 'WhatsappMessenger',
+		des:
+			"A MERN-Stack project that allows you to send the data and save it database using MongoAtlas,  It's appear in front-end page.In this project I used  React in front-end and I used material Ui in Design",
+		tags: [ 'React' , 'Express' , 'mongoDb' ,'CSS' ],
+		links: {
+			// launch: 'https://faizhameed.github.io/landing-page2/.',
+			github: 'https://github.com/ameedasmah/whatsapp_challenge'
+		}
+	},
+];
+
+export default project_list;
